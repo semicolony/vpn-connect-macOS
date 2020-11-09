0031335e2987f4809190faa61cdd07bae0bcd33d
Philipp Kemmeter <philipp.kemmeter@check24.de>
2020-11-09 20:59:30 +0100

    Password optional in connection.ini

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Copy the connection.ini.skel to connection.ini, change mod to 600 and edit its c
     client=openconnect             # This is the client you want to use. See supported clients below.
     uri=dc1.blabla.org             # The destination uri
     username=philipp.kemmeter
-    password=SECRET_PASSWORD_IN_PLAINTEXT # This is why the mod has to be 600
+    password=SECRET_PASSWORD_IN_PLAINTEXT # This is why the mod has to be 600. Optional.
     2fa-append=yes                 # Whether you need the 2fa token to be appended to the password.
     # list of other CL arguments valid for the chosen client
     
--- lib/tunnel.sh
+++ lib/tunnel.sh
@@ -10,7 +10,8 @@ source ./echo.sh
 function tunnel::open {
     local -n tunnel_open_config=$1
 
-    [[ ${tunnel_open_config[2fa-append]} == yes ]] && \
+    [[ ${tunnel_open_config[2fa-append]+exists} ]] && [[ ${tunnel_open_config[2fa-append]} == yes ]] && \
+    [[ ${tunnel_open_config[password]+exists} ]] && [[ ${tunnel_open_config[password]} ]] && \
         tunnel_open_config[password]="${tunnel_open_config[password]}$(tunnel::read_token)"
 
     tunnel::${tunnel_open_config[client]} tunnel_open_config
@@ -20,11 +21,16 @@ function tunnel::openconnect {
     local -n tunnel_openconnect_config=$1
     local additional_args=
 
-    [[ "${tunnel_openconnect_config[authgroup]}" ]] && \
+    [[ ${tunnel_openconnect_config[authgroup]+exists} ]] && [[ ${tunnel_openconnect_config[authgroup]} ]] && \
         additional_args="$additional_args --authgroup ${tunnel_openconnect_config[authgroup]}"
 
-    echo ${tunnel_openconnect_config[password]} | \
-        openconnect -b -u ${tunnel_openconnect_config[username]} --passwd-on-stdin $additional_args ${tunnel_openconnect_config[uri]}
+    if [[ ${tunnel_open_config[password]+exists} ]] && [[ ${tunnel_open_config[password]} ]]; then
+        echo ${tunnel_openconnect_config[password]} | \
+            openconnect -b -u ${tunnel_openconnect_config[username]} --passwd-on-stdin $additional_args ${tunnel_openconnect_config[uri]}
+    else
+        openconnect -b -u ${tunnel_openconnect_config[username]} $additional_args ${tunnel_openconnect_config[uri]}
+    fi
+
 }
 
 function tunnel::read_token {
